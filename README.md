baa33e12025959b84d50e5674cdb133c4da43bb3
jisshub <jissmon476.com>
2022-03-04 20:11:40 +0530

    Todo Delete Task Work

--- README.md
+++ README.md
@@ -22,3 +22,71 @@ export default function Todo() {
 ```
 
 ## Create a state to store the input field value.
+
+- Create a state to store and update the input.
+- In input field, onChange event listener is set to update the state.
+
+```js
+const [task, setTask] = useState('');
+const handleChange = (e) => {
+  setTask(e.target.value);
+}
+return (
+  <div className="todo">
+      <input
+        type="text" 
+        name="text" 
+        id="text" 
+        onChange={(e)=>handleChange(e)} 
+        placeholder="Add todo..."
+      />
+      <button className="">Submit</button>    
+  </div>
+)
+```
+
+## Create onClick event listener to submit the task.
+
+```jsx
+const submitTask = () => {
+  console.log(task)
+}
+
+<button className="" onClick={submitTask}>Submit</button>    
+```
+
+
+## Create state to deal with list of todos
+
+```js
+const [tasks, setTasks] = useState([]);
+const submitTask = () => {
+    if (task) {
+      const taskDetails = {
+        id: Math.floor(Math.random()*1000),
+        task: task,
+        completed: false
+      }
+      setTasks([...tasks, taskDetails]);
+    }
+  }
+  console.log(tasks)
+```
+
+## Show the list of todos in template
+
+```jsx
+{
+  tasks !== [] ? (
+    <ul>
+      {tasks.map((task, index) => (
+        <div id={task.id}>
+          <li className="">{task.taskValue}</li>
+        </div>
+      ))  
+    }
+    </ul>
+  ): null 
+}
+```
+
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,59 @@
+import { useState } from "react"
+
 export default function Todo() {
+
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  }
+  const submitTask = () => {
+    if (task) {
+      const taskDetails = {
+        id: Math.floor(Math.random()*1000),
+        taskValue: task,
+        completed: false
+      }
+
+      setTasks([...tasks, taskDetails]);
+    }
+  }
+  console.log(tasks)
+
+  const handleDelete = (id) => {
+    setTasks((tasks) =>  { 
+      tasks.filter(task => task.id !== id);
+      })
+  }
+
   return (
     <div className="todo">
-        <input type="text" name="text" id="text" placeholder="Add todo..." />
-        <button className="">Submit</button>    
-    </div>
+        <input
+          type="text" 
+          name="text" 
+          id="text" 
+          onChange={(e)=>handleChange(e)} 
+          placeholder="Add todo ..."
+        />
+        <button className="" onClick={submitTask}>Submit</button>    
+          {
+          tasks !== [] ? (
+            <ul>
+              {tasks.map((task, index) => (
+                <div id={task.id}>
+                  <li className="">{task.taskValue}</li>
+                  <button className="delete" 
+                          onClick={() => handleDelete(task.id)}>
+                    Delete
+                  </button>
+                </div>
+              ))  
+            }
+            </ul>
+          ): null 
+          }
 
+    </div>
   )
 }
